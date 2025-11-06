9055efd2df31d7d569c0f5bf491223bd008a951b
dpadrun <dylan.padrun@stud.hslu.ch>
2025-11-06 16:17:46 +0100

    Change pragma solidity version, change class name to token contract

--- blockchain/contracts/VoltazeToken.sol
+++ blockchain/contracts/VoltazeToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
+contract VoltazeToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Voltaze", "VLZ") {
-        _mint(msg.sender, initialSupply); // initialSupply in 18 Decimals
+        _mint(msg.sender, initialSupply);
     }
 }
